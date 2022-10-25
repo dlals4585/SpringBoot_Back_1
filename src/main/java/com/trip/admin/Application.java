dc06db8f7a3010c6fd875a36bae90cfe2c47f6ec
dlals4585 <dlals4585@naver.com>
2022-10-25 23:42:35 +0900

    application.properties 변경4

--- src/main/java/com/trip/admin/Application.java
+++ src/main/java/com/trip/admin/Application.java
@@ -10,4 +10,9 @@ public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
     }
+
+/*    #spring.jpa.properties.hibernate.dialect.storage_engine=innodb
+#spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect
+#spring.datasource.hikari.jdbc-url=jdbc:h2:mem://localhost/~/test;MODE=MYSQL*/
+
 }
