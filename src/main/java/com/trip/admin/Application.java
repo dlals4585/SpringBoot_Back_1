ebba6b706a502b87fc2236a2f5ff74d8218ee629
dlals4585 <dlals4585@naver.com>
2022-10-26 15:36:23 +0900

    application.properties 변경5

--- src/main/java/com/trip/admin/Application.java
+++ src/main/java/com/trip/admin/Application.java
@@ -2,11 +2,6 @@ package com.trip.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.integration.IntegrationDataSourceScriptDatabaseInitializer;
-import org.springframework.boot.sql.init.DatabaseInitializationSettings;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 //@EnableJpaAuditing
 @SpringBootApplication
@@ -15,12 +10,9 @@ public class Application {
         SpringApplication.run(Application.class,args);
     }
 
-    @Bean
+    /*@Bean
     public IntegrationDataSourceScriptDatabaseInitializer customIntegrationDataSourceInitializer(DataSource dataSource) {
         return new IntegrationDataSourceScriptDatabaseInitializer(dataSource, new DatabaseInitializationSettings());
-    }
-/*    #spring.jpa.properties.hibernate.dialect.storage_engine=innodb
-#spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect
-#spring.datasource.hikari.jdbc-url=jdbc:h2:mem://localhost/~/test;MODE=MYSQL*/
+    }*/
 
 }
